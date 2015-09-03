13c69fe98d5dca4e249ff1ef6ac520685db3e8d4
holmgr <viktor.holmgren@gmail.com>
2015-09-03 14:45:37 +0200

    Add lab1 framework

---
+++ lab1/src/life.cpp
@@ -0,0 +1,15 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <iostream>
+#include "grid.h"
+#include "lifeutil.h"
+
+int main() {
+
+    // TODO: Finish the program!
+
+    std::cout << "Have a nice Life!" << std::endl;
+    return 0;
+}
