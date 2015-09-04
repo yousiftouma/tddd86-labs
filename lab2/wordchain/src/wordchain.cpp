4bd58b856d2610c7294bd86778ca374d5858bdf7
Yousif Touma <youto814@student.liu.se>
2015-09-04 15:16:47 +0200

    WORK IN PROGRESS: lab2

--- lab2/wordchain/src/wordchain.cpp
+++ lab2/wordchain/src/wordchain.cpp
@@ -3,9 +3,69 @@
 #include <string>
 #include <queue>
 #include <stack>
+#include <set>
+#include <vector>
+
 using namespace std;
 
 const string alphabet  = "abcdefghijklmnopqrstuvwxyz";
+
+void loadDictionary(ifstream& input, set<string>& dictionary) {
+    while (!input.eof()) {
+        string word;
+        getline(input, word);
+        dictionary.insert(word);
+    }
+}
+
+// Returns all valid neighbouring words (words in the provided dictionary) to a given word
+vector<string> neighbouringWords(string word, set<string> validWords) {
+    vector<string> words = vector<string>();
+    for (int i = 0; i < word.size(); ++i) {
+        for (auto character : alphabet) {
+            string tempWord = word;
+            tempWord[i] = character;
+
+            if (validWords.find(tempWord) != validWords.end() && tempWord[i] != word[i]) {
+                words.push_back(tempWord);
+            }
+        }
+    }
+    return words;
+}
+
+
+stack<string> wordChain(string startWord, string goalWord, set<string> validWords) {
+
+    set<string> alreadyVisited = set<string>();
+
+    queue<stack<string> > wordChains = queue<stack<string> >();
+    stack<string> tempStack = stack<string>();
+    tempStack.push(startWord);
+    wordChains.push(tempStack);
+
+    while (!wordChains.empty()) {
+
+        stack<string> wordStack = wordChains.front();
+        wordChains.pop(); // Remove first elem
+
+        if (wordStack.top() == goalWord) {
+            return wordStack;
+        } else {
+            vector<string> neighbours = neighbouringWords(wordStack.top(), validWords);
+            for (string word : neighbours) {
+                if (alreadyVisited.find(word) == alreadyVisited.end()) {
+                    stack<string> tempWordStack = wordStack;
+                    tempWordStack.push(word);
+                    wordChains.push(tempWordStack);
+                }
+            }
+        }
+
+    }
+    return stack<string>();
+}
+
 
 int main() {
     cout << "Welcome to TDDD86 Word Chain." << endl;
@@ -13,9 +73,22 @@ int main() {
     cout << "first into the second by changing one letter at a time." << endl;
     cout << endl;
 
+    string startWord, goalWord;
     cout << "Please type two words: ";
+    cin >> startWord >> goalWord;
 
-    // TODO: Finish the program!
+    set<string> validWords = set<string>();
+    ifstream dictionaryFile;
+    dictionaryFile.open("dictionary.txt");
+    loadDictionary(dictionaryFile, validWords);
+    dictionaryFile.close();
+
+    auto word = validWords.find("data");
+    cout << validWords.size() << (*word) << endl;
+
+    auto words = neighbouringWords("date", validWords);
+    for (auto x : words)
+        cout << x << endl;
 
     return 0;
 }
