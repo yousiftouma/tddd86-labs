338d110929205b066c63cf49aa2993137fcf0e18
holmgr <viktor.holmgren@gmail.com>
2015-09-06 10:07:24 +0200

    Minor refactor, add some comments

--- lab2/wordchain/src/wordchain.cpp
+++ lab2/wordchain/src/wordchain.cpp
@@ -8,8 +8,15 @@
 
 using namespace std;
 
+// Author: Viktor Holmgren, Yousif Touma
+// A program that calculates and displays the shortest wordchain between two given words (user input).
+// Every word in the chain can only differ by one letter from the preciding and following word, also all words needs to be vaild,
+// i.e inlcuded in the dictionary.txt file.
+
+
 const string alphabet  = "abcdefghijklmnopqrstuvwxyz";
 
+// Loads the specified file contents (inputstream) into the given set
 void loadDictionary(ifstream& input, set<string>& dictionary) {
     while (!input.eof()) {
         string word;
@@ -18,7 +25,7 @@ void loadDictionary(ifstream& input, set<string>& dictionary) {
     }
 }
 
-// Returns all valid neighbouring words (words in the provided dictionary) to a given word
+// Returns all valid neighbouring words (words differing by one character and in the provided dictionary) to a given word
 vector<string> neighbouringWords(string word, set<string>& validWords) {
     vector<string> words = vector<string>();
     for (size_t i = 0; i < word.size(); ++i) {
@@ -35,11 +42,14 @@ vector<string> neighbouringWords(string word, set<string>& validWords) {
 }
 
 
+// Returns a stack of words, i.e the wordchain, if no connection is found an empty stack is returned
 stack<string> wordChain(string startWord, string goalWord, set<string>& validWords) {
 
-    set<string> alreadyVisited = set<string>();
+    set<string> alreadyVisited = set<string>(); // used to prevent infinite cycles from occuring
 
     queue<stack<string> > wordChains = queue<stack<string> >();
+
+    // Setup the starting stack
     stack<string> tempStack = stack<string>();
     tempStack.push(startWord);
     wordChains.push(tempStack);
@@ -67,6 +77,7 @@ stack<string> wordChain(string startWord, string goalWord, set<string>& validWor
     return stack<string>();
 }
 
+// Prints a stack of strings (a wordchain)
 void printResult(stack<string> resultChain) {
 
     if (resultChain.size() == 0) {
@@ -101,7 +112,6 @@ int main() {
     dictionaryFile.close();
 
     printResult(wordChain(startWord, goalWord, validWords));
-
     cout << "Have a nice day!" << endl;
 
     return 0;
