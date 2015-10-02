d50482bca85cb4cb5f0526536d24865d8bee393e
holmgr <viktor.holmgren@gmail.com>
2015-10-02 15:08:02 +0200

    Implement insertSmallest, also some major refactoring

--- lab3/tsp/src/Tour.cpp
+++ lab3/tsp/src/Tour.cpp
@@ -8,6 +8,7 @@
 #include "Tour.h"
 #include "Node.h"
 #include "Point.h"
+#include <limits>
 
 Tour::Tour()
 {
@@ -32,44 +33,40 @@ Tour::Tour(Point a, Point b, Point c, Point d) {
 
 Tour::~Tour()
 {
-    // TODO: write this member
+
 }
 
 void Tour::show()
 {
     if (front == nullptr) {
-        cout << "Empty tour" << endl;
         return;
     }
-
-    Node* current_slow = front;
-    Node* current_fast = front;
+    Node* current = front;
 
     do {
-        cout << current_slow->point.toString() << endl;
-        current_slow = current_slow->next;
-        current_fast = current_fast->next->next;
+        cout << current->point.toString() << endl;
+        current = current->next;
     }
-    while (current_fast != current_slow);
+    while (current != front);
 }
 
 void Tour::draw(QGraphicsScene *scene)
 {
+    scene->clear();
+
     if (front == nullptr) {
         cout << "Empty tour" << endl;
         return;
     }
 
-    Node* current_slow = front;
-    Node* current_fast = front;
+    Node* current = front;
 
     do {
-        current_slow->point.draw(scene);
-        current_slow->point.drawTo(current_slow->next->point, scene);
-        current_slow = current_slow->next;
-        current_fast = current_fast->next->next;
+        current->point.draw(scene);
+        current->point.drawTo(current->next->point, scene);
+        current = current->next;
     }
-    while (current_fast != current_slow);
+    while (current != front);
 }
 
 int Tour::size()
@@ -77,15 +74,13 @@ int Tour::size()
     int size = 0;
 
     if (front != nullptr) {
-        Node* current_slow = front;
-        Node* current_fast = front;
+        Node* current = front;
 
         do {
             ++size;
-            current_slow = current_slow->next;
-            current_fast = current_fast->next->next;
+            current = current->next;
         }
-        while (current_fast != current_slow);
+        while (current != front);
     }
     return size;
 }
@@ -95,22 +90,20 @@ double Tour::distance()
     double dist = 0.0;
 
     if (front != nullptr) {
-        Node* current_slow = front;
-        Node* current_fast = front;
+        Node* current = front;
 
         do {
-            dist += current_slow->point.distanceTo(current_slow->next->point);
-            current_slow = current_slow->next;
-            current_fast = current_fast->next->next;
+            dist += current->point.distanceTo(current->next->point);
+            current = current->next;
         }
-        while (current_fast != current_slow);
+        while (current != front);
     }
     return dist;
 }
 
 void Tour::insertNearest(Point p)
 {
-    double dist = -1;
+    double dist = numeric_limits<double>::infinity();
     Node* nearest = nullptr;
 
     if (front == nullptr) {
@@ -119,20 +112,16 @@ void Tour::insertNearest(Point p)
         front = node;
     }
     else {
-        Node* current_slow = front;
-        Node* current_fast = front;
-
+        Node* current = front;
         do {
-            double current_dist = p.distanceTo(current_slow->point);
-            if (dist == -1 || current_dist < dist) {
+            double current_dist = p.distanceTo(current->point);
+            if (current_dist < dist) {
                 dist = current_dist;
-                nearest = current_slow;
+                nearest = current;
             }
-
-            current_slow = current_slow->next;
-            current_fast = current_fast->next->next;
+            current = current->next;
         }
-        while (current_fast != current_slow);
+        while (current != front);
 
         Node* new_node = new Node(p, nearest->next);
         nearest->next = new_node;
@@ -140,7 +129,43 @@ void Tour::insertNearest(Point p)
 
 }
 
+double insertChange(Node* start, Point p) {
+    double old_dist = start->point.distanceTo(start->next->point);
+    double new_dist = p.distanceTo(start->point) +
+            p.distanceTo(start->next->point);
+    return new_dist - old_dist;
+}
+
 void Tour::insertSmallest(Point p)
 {
-    // TODO: write this member
+    double best_diff = numeric_limits<double>::infinity();
+    Node* best_node = nullptr;
+
+    if (front == nullptr) {
+        Node* node = new Node(p, nullptr);
+        node->next = node;
+        front = node;
+    }
+    else {
+        Node* current = front;
+
+        do {
+
+            double old_dist = current->point.distanceTo(current->next->point);
+            double new_dist = p.distanceTo(current->point) +
+                    p.distanceTo(current->next->point);
+            double current_dist = new_dist - old_dist;
+
+            if (current_dist < best_diff) {
+                best_diff = current_dist;
+                best_node = current;
+            }
+
+            current = current->next;
+        }
+        while (current != front);
+
+        Node* new_node = new Node(p, best_node->next);
+        best_node->next = new_node;
+    }
 }
--- lab3/tsp/src/tsp.cpp
+++ lab3/tsp/src/tsp.cpp
@@ -20,6 +20,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     string filename = "tsp10.txt";
+    //filename = "bier127.txt";
     ifstream input;
     input.open(filename);
 
@@ -43,7 +44,8 @@ int main(int argc, char *argv[]) {
     double y;
     while (input >> x >> y) {
         Point p(x, y);
-        tour.insertNearest(p);
+        //tour.insertNearest(p);
+        tour.insertSmallest(p);
         //uncomment the 4 lines below to animate
         tour.draw(scene);
         std::chrono::milliseconds dura(50);
