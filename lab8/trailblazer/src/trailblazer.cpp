9d43631a69331645b3d8544bd0b0635f4033c6ac
Yousif Touma <youto814@student.liu.se>
2015-12-10 13:51:39 +0100

    Implement DFS

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -8,14 +8,47 @@
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
+vector<Node* > dfsTraverse(BasicGraph& graph, Vertex* start, Vertex* end) {
+
+    start->setColor(GREEN);
+    if (start->visited) {
+        return vector<Node* >();
+
+    }
+    else if (start == end) {
+        vector<Node *> path;
+        path.push_back(end);
+        return path;
+    }
+
+    vector<Node *> path;
+    start->visited = true;
+
+    for (Arc* arc : start->arcs) {
+
+        path = dfsTraverse(graph, arc->finish, end);
+
+        if (!path.empty()) {
+            path.push_back(start);
+            return path;
+        }
+    }
+    start->setColor(GRAY);
+    return path;
+}
+
+
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     // TODO: implement this function; remove these comments
     //       (The function body code provided below is just a stub that returns
     //        an empty vector so that the overall project will compile.
     //        You should remove that code and replace it with your implementation.)
-    vector<Vertex*> path;
+
+    graph.resetData();
+    vector<Vertex*> path = dfsTraverse(graph, start, end);
     return path;
 }
+
 
 vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     // TODO: implement this function; remove these comments
